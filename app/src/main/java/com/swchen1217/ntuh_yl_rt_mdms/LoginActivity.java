4bc46940f815a586e950ca9a83c9bb0b5933d8c7
swchen1217 <swchen1217@gmail.com>
2019-06-07 21:28:06 +0800

    new

---
+++ app/src/main/java/com/swchen1217/ntuh_yl_rt_mdms/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.swchen1217.ntuh_yl_rt_mdms;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
