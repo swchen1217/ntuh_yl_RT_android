6fa494edfb29226d1898e22eec4c9c2a1218517b
swchen1217 <swchen1217@gmail.com>
2019-06-07 19:20:54 +0800

    20190607

---
+++ app/src/main/java/com/swchen1217/ntuh_yl_rt_mams/MainActivity.java
@@ -0,0 +1,13 @@
+package com.swchen1217.ntuh_yl_rt_mams;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
