e7380b6c94898b6f7c8a1845f39c267cd51862d8
swchen1217 <swchen1217@gmail.com>
2019-06-08 00:59:16 +0800

    re

--- app/src/main/java/com/swchen1217/ntuh_yl_rt_mdms/LoginActivity.java
+++ app/src/main/java/com/swchen1217/ntuh_yl_rt_mdms/LoginActivity.java
@@ -2,12 +2,22 @@ package com.swchen1217.ntuh_yl_rt_mdms;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
-
+    EditText et_acc,et_pw;
+    Button btn_forget,btn_login;
+    CheckBox cb_rememberme;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        et_acc=findViewById(R.id.et_acc);
+        et_pw=findViewById(R.id.et_pw);
+        btn_forget=findViewById(R.id.btn_forgetpw);
+        btn_login=findViewById(R.id.btn_login);
+        cb_rememberme=findViewById(R.id.cb_rememberme);
     }
 }
