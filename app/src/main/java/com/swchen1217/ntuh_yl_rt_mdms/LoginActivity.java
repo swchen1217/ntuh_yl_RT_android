5be90e8750018a9b1510a9a41818f1f58d09686f
swchen1217 <swchen1217@gmail.com>
2019-06-09 17:50:51 +0800

    re

--- app/src/main/java/com/swchen1217/ntuh_yl_rt_mdms/LoginActivity.java
+++ app/src/main/java/com/swchen1217/ntuh_yl_rt_mdms/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.swchen1217.ntuh_yl_rt_mdms;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,8 +13,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class LoginActivity extends AppCompatActivity {
+    private static final String TAG ="debug";
     EditText et_acc,et_pw;
     Button btn_forget,btn_login;
     CheckBox cb_rememberme;
@@ -29,57 +40,14 @@ public class LoginActivity extends AppCompatActivity {
         btn_login.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(engineering_mode_nologin==true && et_acc.getText().toString().equals("") && et_pw.getText().toString().equals("")){
-                    startActivity(new Intent(LoginActivity.this,MainActivity.class));
-                    finish();
-                }else{
-                    if(et_acc.getText().toString().equals("") || et_pw.getText().toString().equals("")){
-                        new AlertDialog.Builder(LoginActivity.this)
-                                .setTitle("帳號或密碼為空白!!")
-                                .setPositiveButton("確定", new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialog, int which) {
-
-                                    }
-                                })
-                                .show();
+                if(isConnected()){
+                    if(engineering_mode_nologin==true && et_acc.getText().toString().equals("") && et_pw.getText().toString().equals("")){
+                        startActivity(new Intent(LoginActivity.this,MainActivity.class));
+                        finish();
                     }else{
-                        if(et_acc.getText().toString().equals("admin")){
-                            if(et_pw.getText().toString().equals("admin")){
-                                login_error_count=0;
-                                startActivity(new Intent(LoginActivity.this,MainActivity.class));
-                                finish();
-                            }else{
-                                login_error_count++;
-                                if(login_error_count!=3){
-                                    new AlertDialog.Builder(LoginActivity.this)
-                                            .setTitle("密碼錯誤!!")
-                                            .setMessage("還有 "+(3-login_error_count)+" 次機會")
-                                            .setPositiveButton("確定", new DialogInterface.OnClickListener() {
-                                                @Override
-                                                public void onClick(DialogInterface dialog, int which) {
-
-                                                }
-                                            })
-                                            .show();
-                                    et_pw.setText("");
-                                }else{
-                                    new AlertDialog.Builder(LoginActivity.this)
-                                            .setTitle("密碼錯誤!!")
-                                            .setMessage("密碼錯誤 3 次")
-                                            .setPositiveButton("確定", new DialogInterface.OnClickListener() {
-                                                @Override
-                                                public void onClick(DialogInterface dialog, int which) {
-                                                    finish();
-                                                }
-                                            })
-                                            .show();
-                                }
-                            }
-                        }else{
+                        if(et_acc.getText().toString().equals("") || et_pw.getText().toString().equals("")){
                             new AlertDialog.Builder(LoginActivity.this)
-                                    .setTitle("員工編號錯誤!!")
-                                    .setMessage("此員工編號尚未註冊")
+                                    .setTitle("帳號或密碼為空白!!")
                                     .setPositiveButton("確定", new DialogInterface.OnClickListener() {
                                         @Override
                                         public void onClick(DialogInterface dialog, int which) {
@@ -87,12 +55,67 @@ public class LoginActivity extends AppCompatActivity {
                                         }
                                     })
                                     .show();
-                            et_acc.setText("");
-                            et_pw.setText("");
+                        }else{
+                            if(et_acc.getText().toString().equals("admin")){
+                                if(et_pw.getText().toString().equals("admin")){
+                                    login_error_count=0;
+                                    startActivity(new Intent(LoginActivity.this,MainActivity.class));
+                                    finish();
+                                }else{
+                                    login_error_count++;
+                                    if(login_error_count!=3){
+                                        new AlertDialog.Builder(LoginActivity.this)
+                                                .setTitle("密碼錯誤!!")
+                                                .setMessage("還有 "+(3-login_error_count)+" 次機會")
+                                                .setPositiveButton("確定", new DialogInterface.OnClickListener() {
+                                                    @Override
+                                                    public void onClick(DialogInterface dialog, int which) {
+
+                                                    }
+                                                })
+                                                .show();
+                                        et_pw.setText("");
+                                    }else{
+                                        new AlertDialog.Builder(LoginActivity.this)
+                                                .setTitle("密碼錯誤!!")
+                                                .setMessage("密碼錯誤 3 次")
+                                                .setPositiveButton("確定", new DialogInterface.OnClickListener() {
+                                                    @Override
+                                                    public void onClick(DialogInterface dialog, int which) {
+                                                        finish();
+                                                    }
+                                                })
+                                                .show();
+                                    }
+                                }
+                            }else{
+                                new AlertDialog.Builder(LoginActivity.this)
+                                        .setTitle("員工編號錯誤!!")
+                                        .setMessage("此員工編號尚未註冊")
+                                        .setPositiveButton("確定", new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+
+                                            }
+                                        })
+                                        .show();
+                                et_acc.setText("");
+                                et_pw.setText("");
+                            }
                         }
                     }
+                }else{
+                    Toast.makeText(LoginActivity.this, "無法連接至網際網路", Toast.LENGTH_SHORT).show();
                 }
             }
         });
+    }
+    private boolean isConnected(){
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            return true;
+        }
+        return false;
     }
 }
--- app/src/main/java/com/swchen1217/ntuh_yl_rt_mdms/MainActivity.java
+++ app/src/main/java/com/swchen1217/ntuh_yl_rt_mdms/MainActivity.java
@@ -1,16 +1,16 @@
 package com.swchen1217.ntuh_yl_rt_mdms;
 
-import android.os.StrictMode;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.ProtocolException;
 import java.net.URL;
 
 import javax.net.ssl.HttpsURLConnection;
@@ -18,24 +18,46 @@ import javax.net.ssl.HttpsURLConnection;
 public class MainActivity extends AppCompatActivity {
 
     TextView tv;
+    String output="no change";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv=findViewById(R.id.tv);
+        connection_server("index","in=in&in2=intwo");
+        tv.setText(output);
+    }
+    /*Handler mHandler = new Handler() {
+        @Override    public void handleMessage(Message msg) {
+            switch(msg.what)
+            {
+                case 1:
+
+            }
+            super.handleMessage(msg);
+        }
+    };*/
+    void connection_server(final String urlfile, final String poststr){
         new Thread()
         {
+            private String output;
+
             public void run()
             {
-                String urlString = "http://swchen1217.ddns.net/ntuh_yl_RT_mdms_php/index.php?in=test";
+                String urlString = "http://swchen1217.ddns.net/ntuh_yl_RT_mdms_php/"+urlfile+".php";
                 HttpURLConnection connection = null;
                 try {
                     URL url = new URL(urlString);
                     connection = (HttpURLConnection) url.openConnection();
+                    connection.setDoOutput(true);
+                    connection.setDoInput(true);
+                    connection.setRequestMethod("POST");
                     connection.setReadTimeout(1500);
                     connection.setConnectTimeout(1500);
                     connection.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.71 Safari/537.36");
                     connection.setInstanceFollowRedirects(true);
+                    DataOutputStream dos = new DataOutputStream(connection.getOutputStream());
+                    dos.writeBytes(poststr);
                     if( connection.getResponseCode() == HttpsURLConnection.HTTP_OK ){
                         try(BufferedReader br = new BufferedReader(
                                 new InputStreamReader(connection.getInputStream(), "utf-8"))) {
@@ -44,31 +66,20 @@ public class MainActivity extends AppCompatActivity {
                             while ((responseLine = br.readLine()) != null) {
                                 response.append(responseLine.trim());
                             }
-                            runOnUiThread(new Runnable() {
+                            /*Message message;
+                            message = mHandler.obtainMessage(1,response.toString());
+                            mHandler.sendMessage(message);*/
+                            //update(response.toString());
+                            output=response.toString();
+                            /*runOnUiThread(new Runnable() {
                                 @Override
                                 public void run() {
                                     // update TextView here!
                                     tv.setText(response.toString());
+
                                 }
-                            });
+                            });*/
                         }
-                        /*InputStream inputStream= connection.getInputStream();
-                        BufferedReader bufferedReader  = new BufferedReader( new InputStreamReader(inputStream) );
-                        String tempStr;
-                        StringBuffer stringBuffer = new StringBuffer();
-                        while( ( tempStr = bufferedReader.readLine() ) != null ) {
-                            stringBuffer.append( tempStr );
-                        }
-                        bufferedReader.close();
-                        inputStream.close();
-                        final String responseString = stringBuffer.toString();
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                // update TextView here!
-                                tv.setText(responseString);
-                            }
-                        });*/
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -78,7 +89,11 @@ public class MainActivity extends AppCompatActivity {
                         connection.disconnect();
                     }
                 }
+
             }
         }.start();
     }
+    /*void update(String in){
+        output=in;
+    }*/
 }
