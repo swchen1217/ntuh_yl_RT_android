5876b27cbcc240e8d625a0a51b4c8587feb74cce
swchen1217 <swchen1217@gmail.com>
2019-06-15 15:10:39 +0800

    re

--- app/src/main/java/com/swchen1217/ntuh_yl_rt_mdms/MenuActivity.java
+++ app/src/main/java/com/swchen1217/ntuh_yl_rt_mdms/MenuActivity.java
@@ -2,12 +2,56 @@ package com.swchen1217.ntuh_yl_rt_mdms;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
-
+    Button btn_1,btn_2,btn_3,btn_4,btn_5,btn_6;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        btn_1=findViewById(R.id.btn_menu_1);
+        btn_2=findViewById(R.id.btn_menu_2);
+        btn_3=findViewById(R.id.btn_menu_3);
+        btn_4=findViewById(R.id.btn_menu_4);
+        btn_5=findViewById(R.id.btn_menu_5);
+        btn_6=findViewById(R.id.btn_menu_6);
+        btn_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+        btn_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btn_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btn_4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btn_5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btn_6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
