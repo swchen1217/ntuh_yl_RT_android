5e0fa17717304c79e88738bdc05e8099b3220d27
swchen1217 <swchen1217@gmail.com>
2019-06-20 00:27:31 +0800

    re

--- app/src/main/java/com/swchen1217/ntuh_yl_rt_mdms/UpdateStatusActivity.java
+++ app/src/main/java/com/swchen1217/ntuh_yl_rt_mdms/UpdateStatusActivity.java
@@ -33,13 +33,14 @@ public class UpdateStatusActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 IntentIntegrator integrator = new IntentIntegrator(UpdateStatusActivity.this);
-                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
                 integrator.addExtra("SCAN_MODE", "QR_CODE_MODE");
                 integrator.setPrompt("現況登錄 QR Code 掃描");
                 integrator.setCameraId(0);
                 integrator.setBeepEnabled(true);
                 integrator.setBarcodeImageEnabled(false);
                 integrator.setOrientationLocked(true);
+                //integrator.setTimeout(10000);
                 integrator.initiateScan();
             }
         });
