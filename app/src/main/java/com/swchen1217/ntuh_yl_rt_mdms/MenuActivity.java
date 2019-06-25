6468a2b9b9b0ddaad52406d50b7f15c57168c48f
swchen1217 <swchen1217@gmail.com>
2019-06-25 13:25:40 +0800

    re

--- app/src/main/java/com/swchen1217/ntuh_yl_rt_mdms/MenuActivity.java
+++ app/src/main/java/com/swchen1217/ntuh_yl_rt_mdms/MenuActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -73,5 +75,26 @@ public class MenuActivity extends AppCompatActivity {
         }
         return super.onKeyDown(keyCode, event);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logout:
+                startActivity(new Intent(this, LoginActivity.class));
+                finish();
+                return true;
+            /*case R.id.exit:
+                closeBT();
+                finish();*/
+        }
+        return false;
     }
 }
