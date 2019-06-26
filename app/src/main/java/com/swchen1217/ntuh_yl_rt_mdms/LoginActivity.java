a51c620902d615d2ba6e81652bfeadc90750fc77
swchen1217 <swchen1217@gmail.com>
2019-06-26 14:42:08 +0800

    add "www"

--- app/src/main/java/com/swchen1217/ntuh_yl_rt_mdms/LoginActivity.java
+++ app/src/main/java/com/swchen1217/ntuh_yl_rt_mdms/LoginActivity.java
@@ -381,7 +381,7 @@ public class LoginActivity extends AppCompatActivity {
                         .post(formBody) // 使用post連線
                         .build();*/
                 Request request = new Request.Builder()
-                        .url("https://google.com/")
+                        .url("https://www.google.com/")
                         .build();
                 Call call = client.newCall(request);
                 try (Response response = call.execute()) {
