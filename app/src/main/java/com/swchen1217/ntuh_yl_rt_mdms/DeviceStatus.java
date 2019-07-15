316f9714ce75149357fc5e6fd898b1530109b616
Sz-Wei Chen <swchen1217@gmail.com>
2019-07-15 17:51:29 +0800

    new

---
+++ app/src/main/java/com/swchen1217/ntuh_yl_rt_mdms/DeviceStatus.java
@@ -0,0 +1,10 @@
+package com.swchen1217.ntuh_yl_rt_mdms;
+
+public interface DeviceStatus {
+    int STATUS_DISABLE=-1;
+    int STATUS_NOT_ENABLED=0;
+    int STATUS_USE=1;
+    int STATUS_STOCK=2;
+    int STATUS_FIX=3;
+    int STATUS_MAINTENANCE_CHECK=4;
+}
---
+++ app/src/main/java/com/swchen1217/ntuh_yl_rt_mdms/Permission.java
@@ -0,0 +1,12 @@
+package com.swchen1217.ntuh_yl_rt_mdms;
+
+public interface Permission {
+    int PERMISSINO_DELETE=-1;
+    int PERMISSINO_NOT_ENABLED=0;
+    int PERMISSINO_INQUIRT_STARUS=1;
+    int PERMISSINO_UPDATE_STATUS=2;
+    int PERMISSINO_READ_LOG=3;
+    int PERMISSINO_MANAGE_DEVICE=4;
+    int PERMISSINO_MANAGE_USER=5;
+    int PERMISSINO_ROOT=9;
+}
