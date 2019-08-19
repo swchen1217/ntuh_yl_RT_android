e5f46c61b91296bb410ae2f886ff0d7f085ec533
Sz-Wei Chen <swchen1217@gmail.com>
2019-08-19 17:22:28 +0800

    re

---
+++ app/src/main/java/DeviceTable.java
@@ -0,0 +1,20 @@
+public class DeviceTable {
+    public DeviceTable(String DID, String category, String model, String number, String user, String position, String status, String LastModified) {
+        this.DID = DID;
+        this.category = category;
+        this.model = model;
+        this.number = number;
+        this.user = user;
+        this.position = position;
+        this.status = status;
+        this.LastModified = LastModified;
+    }
+    private String DID;
+    private String category;
+    private String model;
+    private String number;
+    private String user;
+    private String position;
+    private String status;
+    private String LastModified;
+}
