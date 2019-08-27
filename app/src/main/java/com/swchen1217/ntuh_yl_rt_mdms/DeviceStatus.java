51d13c20f1e33284c08ce7b3dd34fab6d43f003e
Sz-Wei Chen <swchen1217@gmail.com>
2019-08-28 01:19:09 +0800

    re

--- app/src/main/java/com/swchen1217/ntuh_yl_rt_mdms/DeviceStatus.java
+++ app/src/main/java/com/swchen1217/ntuh_yl_rt_mdms/DeviceStatus.java
@@ -1,14 +1,13 @@
 package com.swchen1217.ntuh_yl_rt_mdms;
 
 public interface DeviceStatus {
-    int STATUS_DISABLE = -1;
-    int STATUS_NULL = 0;
-    int STATUS_USE = 1;
-    int STATUS_STOREROOM = 2;
-    int STATUS_FIX = 3;
-    int STATUS_MAINTENANCE_CHECK = 4;
-    String[] StatusStr = {"停用",
-            "無狀態",
+    //int STATUS_DISABLE = -1; //停用
+    int STATUS_NULL = 0; //無狀態
+    int STATUS_USE = 1; //使用中
+    int STATUS_STOREROOM = 2; //倉庫
+    int STATUS_FIX = 3; //維修中
+    int STATUS_MAINTENANCE_CHECK = 4; //保養中
+    String[] StatusStr = {"無狀態",
             "使用中",
             "倉庫",
             "維修中",
